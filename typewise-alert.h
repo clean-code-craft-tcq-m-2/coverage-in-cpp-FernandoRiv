e8bbb9b4aa4c7d02a1f1440924f782eba702e00b
Fernando <fer96nando@gmail.com>
2023-01-16 23:37:10 -0500

    [Commit 1] Ensure environment variable push works
    Grouping the enums of the header file into a cohesive block.

--- typewise-alert.h
+++ typewise-alert.h
@@ -12,13 +12,13 @@ typedef enum {
   TOO_HIGH
 } BreachType;
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
-
 typedef enum {
   TO_CONTROLLER,
   TO_EMAIL
 } AlertTarget;
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 
 typedef struct {
   CoolingType coolingType;
